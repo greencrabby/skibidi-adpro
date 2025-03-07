070490e9846eea24847ff88ce430bd56ebcdf61f
Joshua ESS <158529708+greencrabby@users.noreply.github.com>
2025-03-07 16:40:55 +0700

    [RED] Add tests for Payment Service.

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImplTest.java
@@ -0,0 +1,76 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceImplTest {
+    @InjectMocks
+    PaymentServiceImplTest paymentService;
+    @Mock
+    PaymentRepository paymentRepository;
+
+    List<Payment> payments;
+    Order order;
+    @BeforeEach
+    void setUp() {
+        List<Product> products = new ArrayList<>();
+        Product product1 = new Product();
+        products.add(product1);
+
+        order = new Order("123456789-skibidi-order-id", products, 128560000L, "Sigma");
+
+        payments = new ArrayList<>();
+        Map<String, String> paymentData1 = new HashMap<>();
+        paymentData1.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment1 = new Payment("6789-skibidi12345", PaymentMethod.VOUCHER.getValue(), order, paymentData1);
+        payments.add(payment1);
+
+        Map<String, String> paymentData2 = new HashMap<>();
+        paymentData2.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment2 = new Payment("6789-skibidi12345", PaymentMethod.VOUCHER.getValue(), order, paymentData2);
+        payments.add(payment2);
+    }
+
+    @Test
+    void testCreatePayment() {
+        Payment payment = payments.get(1);
+        doReturn(payment).when(paymentRepository).save(payment);
+
+        Map<String, String> paymentData = new HashMap<>();
+        paymentData.put("voucherCode", "ESHOP1234ABC5678");
+        Payment newPayment = paymentService.addPayment(order, PaymentMethod.VOUCHER.getValue(), paymentData);
+        verify(paymentRepository, times(1)).save(payment);
+        Payment result = paymentService.getPayment(newPayment.getId());
+        assertEquals(result.getId(), newPayment.getId());
+    }
+
+    @Test
+    void testSetStatus() {
+        Payment payment = payments.get(1);
+        Payment newPayment = new Payment(payment.getId(), payment.getMethod(), payment.getOrder(), payment.getPaymentData());
+        doReturn(payment).when(paymentRepository).findById(payment.getId());
+        doReturn(newPayment).when(paymentRepository).save(any(Payment.class));
+
+        Payment result = paymentService.setStatus(payment, PaymentStatus.SUCCESS.getValue());
+
+        assertEquals(payment.getId(), result.getId());
+        assertEquals(PaymentStatus.SUCCESS.getValue(), result.getStatus());
+        verify(paymentRepository, times(1)).save(any(Payment.class));
+    }
+}
